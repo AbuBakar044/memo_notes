205e063cf7dec14eb393cfdbfa419181d103f84b
AbuBakar044 <bhattiabubakar@gmail.com>
2023-07-20 14:04:41 +0500

    20-07-2023 Class
    How to Get Data From hive DB with ValueListenableBuilder
    
    How to add App Icon
    How to change app name
    How to build release version

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:memo_notes/models/note.dart';
 import 'package:memo_notes/screens/add_notes_screen.dart';
 import 'package:memo_notes/widgets/custom_drawer.dart';
@@ -18,7 +19,7 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   void initState() {
     notesBox = Hive.box<NoteModel>('notes');
-    getDataFromDatabase();
+    //getDataFromDatabase();
     super.initState();
   }
 
@@ -45,37 +46,42 @@ class _HomeScreenState extends State<HomeScreen> {
       body: Container(
         height: MediaQuery.of(context).size.height,
         width: MediaQuery.of(context).size.width,
-        child: ListView.builder(
-          itemCount: notesList.length,
-          itemBuilder: (context, index) {
-            return Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: ListTile(
-                tileColor: Colors.yellow,
-                title: Text('${notesList[index].title}'),
-                subtitle: Text('${notesList[index].note}'),
-                trailing: IconButton(
-                  onPressed: () {
-                    deleteNote(index);
-                  },
-                  icon: Icon(Icons.delete),
-                ),
-              ),
-            );
-          },
-        ),
+        child: ValueListenableBuilder(
+            valueListenable: notesBox!.listenable(),
+            builder: (context, _, __) {
+              notesList = notesBox!.values.toList().cast<NoteModel>();
+              return ListView.builder(
+                itemCount: notesList.length,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ListTile(
+                      tileColor: Colors.yellow,
+                      title: Text('${notesList[index].title}'),
+                      subtitle: Text('${notesList[index].note}'),
+                      trailing: IconButton(
+                        onPressed: () {
+                          deleteNote(index);
+                        },
+                        icon: Icon(Icons.delete),
+                      ),
+                    ),
+                  );
+                },
+              );
+            }),
       ),
     );
   }
 
-  Future getDataFromDatabase() async {
-    await Hive.openBox<NoteModel>('notes');
+  // Future getDataFromDatabase() async {
+  //   await Hive.openBox<NoteModel>('notes');
 
-    notesList = notesBox!.values.toList().cast<NoteModel>();
-    setState(() {});
+  //   notesList = notesBox!.values.toList().cast<NoteModel>();
+  //   setState(() {});
 
-    print('...................${notesList.length}');
-  }
+  //   print('...................${notesList.length}');
+  // }
 
   void deleteNote(int index) {
     notesList.removeAt(index);
